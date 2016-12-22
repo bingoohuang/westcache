43868390963c2ccee1af3c5f0979f49cec880b7b
bingoohuang <bingoo.huang@gmail.com>
2016-12-22 15:54:16 +0800

    lombok UtilityClass

--- src/main/java/com/github/bingoohuang/westcache/WestCache.java
+++ src/main/java/com/github/bingoohuang/westcache/WestCache.java
@@ -2,30 +2,30 @@ package com.github.bingoohuang.westcache;
 
 import com.github.bingoohuang.westcache.cachemanager.WestCacheManagerBuilder;
 import com.google.common.base.Optional;
+import lombok.experimental.UtilityClass;
 
 import java.util.concurrent.Callable;
 
 /**
  * @author bingoohuang [bingoohuang@gmail.com] Created on 2016/12/22.
  */
+@UtilityClass
 public class WestCache {
-    static WestCacheManager manager = WestCacheManagerBuilder.buildGuavaCacheManager();
+    WestCacheManager manager = WestCacheManagerBuilder.buildGuavaCacheManager();
 
-    public static <T> Optional<T> get(
-            String cacheKey, Callable<Optional<T>> callable) {
+    public <T> Optional<T> get(String cacheKey, Callable<Optional<T>> callable) {
         return manager.get(cacheKey, callable);
     }
 
-    public static <T> Optional<T> get(String cacheKey) {
+    public <T> Optional<T> get(String cacheKey) {
         return manager.get(cacheKey);
     }
 
-    public static <T> Optional<T> getSnapshot(
-            String cacheKey, Callable<Optional<T>> callable) {
+    public <T> Optional<T> getSnapshot(String cacheKey, Callable<Optional<T>> callable) {
         return manager.getSnapshot(cacheKey, callable);
     }
 
-    public static <T> void put(String cacheKey, Optional<T> cacheValue) {
+    public <T> void put(String cacheKey, Optional<T> cacheValue) {
         manager.put(cacheKey, cacheValue);
     }
 }
