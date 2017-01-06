a62b9fbf31466584a3be713fa4c1a4da49fd7750
bingoohuang <bingoo.huang@gmail.com>
2017-01-06 20:35:54 +0800

    custom annotation attribute expireAfterWrite test

---
+++ src/test/java/com/github/bingoohuang/westcache/RedisManagerExpireCustomAnnTest.java
@@ -0,0 +1,44 @@
+package com.github.bingoohuang.westcache;
+
+import lombok.SneakyThrows;
+import org.junit.Test;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static com.google.common.truth.Truth.assertThat;
+
+/**
+ * @author bingoohuang [bingoohuang@gmail.com] Created on 2017/1/6.
+ */
+public class RedisManagerExpireCustomAnnTest {
+    @Retention(RetentionPolicy.RUNTIME)
+    @WestCacheable(manager = "redis", keyer = "simple")
+    @Target({ElementType.METHOD, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+    public @interface RedisCache {
+        String expireAfterWrite();
+    }
+
+    public static volatile int calledTimes = 0;
+
+    public static class RedisCacheService {
+        @RedisCache(expireAfterWrite = "1s")
+        public long getTimestamp() {
+            ++calledTimes;
+            return System.currentTimeMillis();
+        }
+    }
+
+    static RedisCacheService service = WestCacheFactory.create(RedisCacheService.class);
+
+    @Test @SneakyThrows
+    public void test() {
+        service.getTimestamp();
+        service.getTimestamp();
+        Thread.sleep(1000L);
+        service.getTimestamp();
+        assertThat(calledTimes).isEqualTo(2);
+    }
+}
