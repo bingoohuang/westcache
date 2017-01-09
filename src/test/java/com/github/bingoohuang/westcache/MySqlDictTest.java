a42523af21627a19833aa7dfb2a3ad9862c1765b
bingoohuang <bingoo.huang@gmail.com>
2017-01-09 10:36:15 +0800

    import jmockit in unit

--- src/test/java/com/github/bingoohuang/westcache/MySqlDictTest.java
+++ src/test/java/com/github/bingoohuang/westcache/MySqlDictTest.java
@@ -1,9 +1,13 @@
 package com.github.bingoohuang.westcache;
 
+import com.github.bingoohuang.westcache.base.WestCacheFlusher;
 import com.google.common.collect.Lists;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
+import mockit.Capturing;
+import mockit.Expectations;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.n3r.eql.eqler.EqlerFactory;
@@ -14,6 +18,7 @@ import java.sql.Timestamp;
 import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 import static com.github.bingoohuang.westcache.WestCacheRegistry.flusherRegistry;
@@ -32,21 +37,20 @@ public class MySqlDictTest {
 
     @EqlerConfig
     public interface DictDao {
-        @Sql("drop table if exists cache_dict;" +
-
-                "create table cache_dict(" +
-                "  id int, " +
-                "  name varchar(100), " +
-                "  addr varchar(100), " +
-                "  update_time timestamp on update current_timestamp default current_timestamp" +
-                ")engine=innodb default charset=utf8;" +
-
-                "insert into cache_dict(id, name, addr) " +
-                "values(1, 'bingoo', '南京')," +
-                "      (2, 'dingoo', '北京')," +
-                "      (3, 'pingoo', '上海')," +
-                "      (4, 'qingoo', '广州')")
-        void setup();
+        /*
+        drop table if exists cache_dict;
+        create table cache_dict(
+          id int,
+          name varchar(100),
+          addr varchar(100),
+          update_time timestamp on update current_timestamp default current_timestamp
+        )engine=innodb default charset=utf8;
+        insert into cache_dict(id, name, addr);
+        values(1, 'bingoo', '南京'),
+              (2, 'dingoo', '北京'),
+              (3, 'pingoo', '上海'),
+              (4, 'qingoo', '广州')");
+        */
 
         @Sql("select id, name, addr from cache_dict")
         List<CacheDictBean> selectAll();
@@ -54,50 +58,57 @@ public class MySqlDictTest {
         @Sql("select max(update_time) max from cache_dict")
         Timestamp getMaxUpdateTime();
 
-        @Sql("select now()")
-        Timestamp selectNow();
-
-        @Sql("update cache_dict set addr = #addr# where id = #id#")
-        int updateCacheDict(CacheDictBean bean);
+//        @Sql("update cache_dict set addr = #addr# where id = #id#")
+//        int updateCacheDict(CacheDictBean bean);
     }
 
-    static DictDao dictDao = EqlerFactory.getEqler(DictDao.class);
+    //    static DictDao dictDao = EqlerFactory.getEqler(DictDao.class);
     static ScheduledExecutorService scheduledExecutor;
     static volatile long updateCheckTime;
-    static volatile long setupTimeSeconds;
+    static WestCacheFlusher flusher = flusherRegistry.get("simple");
+    private ScheduledFuture<?> scheduledFuture;
+
+    @Capturing DictDao anyDictDao;
 
     @BeforeClass
     public static void beforeClass() {
-        dictDao.setup();
-        setupTimeSeconds = dictDao.selectNow().getTime() / 1000;
-
         scheduledExecutor = Executors.newSingleThreadScheduledExecutor();
-        scheduledExecutor.scheduleAtFixedRate(new Runnable() {
+    }
+
+    @Before
+    public void before() {
+        if (scheduledFuture != null) return;
+
+        new Expectations() {{
+            anyDictDao.getMaxUpdateTime();
+            result = null;
+        }};
+
+        scheduledFuture = scheduledExecutor.scheduleAtFixedRate(new Runnable() {
             Long lastMax = 0L;
 
             @Override public void run() {
                 Timestamp max = dictDao.getMaxUpdateTime();
                 if (max == null && lastMax == null) return;
-                if (max != null && max.getTime() == lastMax) return;
+                if (max != null && lastMax != null && max.getTime() == lastMax)
+                    return;
 
                 log.debug("lastMax:{}, max:{}, updateCheckTime:{}", lastMax, max, updateCheckTime);
 
-                if (lastMax != null && lastMax > 0L) {
-                    val flusher = flusherRegistry.get("simple");
-                    flusher.flush(null, "mysql.cache_dicts", "");
-                    updateCheckTime = System.currentTimeMillis();
-                    log.debug("update updateCheckTime:{}", updateCheckTime);
-                }
-
+                flusher.flush(null, "mysql.cache_dicts", "");
+                updateCheckTime = System.currentTimeMillis();
+                log.debug("update updateCheckTime:{}", updateCheckTime);
                 lastMax = max == null ? null : max.getTime();
             }
-        }, 0, 200, TimeUnit.MILLISECONDS);
+        }, 0, 50, TimeUnit.MILLISECONDS);
     }
 
     @AfterClass
     public static void afterClass() {
         scheduledExecutor.shutdownNow();
     }
+
+    static DictDao dictDao = EqlerFactory.getEqler(DictDao.class);
 
     public static class CacheDictService {
         @WestCacheable(key = "mysql.cache_dicts", flusher = "simple")
@@ -109,40 +120,51 @@ public class MySqlDictTest {
     @Test @SneakyThrows
     public void dictCache() {
         val service = WestCacheFactory.create(CacheDictService.class);
-
-        val cacheDicts = service.getCacheDicts();
         val beans = Lists.newArrayList(
                 new CacheDictBean(1, "bingoo", "南京"),
                 new CacheDictBean(2, "dingoo", "北京"),
                 new CacheDictBean(3, "pingoo", "上海"),
                 new CacheDictBean(4, "qingoo", "广州")
         );
-        assertThat(cacheDicts).isEqualTo(beans);
+        val point1 = System.currentTimeMillis();
+        long lastUpdateCheckTime = updateCheckTime;
 
+        new Expectations() {{
+            anyDictDao.getMaxUpdateTime();
+            result = new Timestamp(point1 + 100);
+            anyDictDao.selectAll();
+            result = beans;
+        }};
+
+        do {
+            Thread.sleep(50L);
+        } while (updateCheckTime == lastUpdateCheckTime);
+
+        val cacheDicts = service.getCacheDicts();
+        assertThat(cacheDicts).isEqualTo(beans);
         val cacheDicts2 = service.getCacheDicts();
         assertThat(cacheDicts).isSameAs(cacheDicts2);
 
-        // 数据库里面的时间，精度只到秒，所以这里必须确保更新时，时间已经过了1秒
-        do {
-            Thread.sleep(50L);
-        } while (dictDao.selectNow().getTime() / 1000 <= setupTimeSeconds);
-
-        long lastUpdateCheckTime = updateCheckTime;
-        dictDao.updateCacheDict(new CacheDictBean(1, "bingoo", "蓝鲸"));
-
-        do {
-            Thread.sleep(50);
-        } while (lastUpdateCheckTime == updateCheckTime);
-
-        val cacheDicts3 = service.getCacheDicts();
-        assertThat(cacheDicts3).isNotSameAs(cacheDicts2);
-
-        val beans3 = Lists.newArrayList(
+        val beans2 = Lists.newArrayList(
                 new CacheDictBean(1, "bingoo", "蓝鲸"),
                 new CacheDictBean(2, "dingoo", "北京"),
                 new CacheDictBean(3, "pingoo", "上海"),
                 new CacheDictBean(4, "qingoo", "广州")
         );
-        assertThat(cacheDicts3).isEqualTo(beans3);
+        lastUpdateCheckTime = updateCheckTime;
+        new Expectations() {{
+            anyDictDao.getMaxUpdateTime();
+            result = new Timestamp(point1 + 200);
+            anyDictDao.selectAll();
+            result = beans2;
+        }};
+
+        do {
+            Thread.sleep(50L);
+        } while (updateCheckTime == lastUpdateCheckTime);
+
+        val cacheDicts3 = service.getCacheDicts();
+        assertThat(cacheDicts3).isNotSameAs(cacheDicts2);
+        assertThat(cacheDicts3).isEqualTo(beans2);
     }
 }
