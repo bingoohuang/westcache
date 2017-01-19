f51faf31db5c862de9c48f6996fd78721b65efec
bingoohuang <bingoo.huang@gmail.com>
2017-01-19 15:57:05 +0800

    remove see in ExpireAfterWrite

--- src/main/java/com/github/bingoohuang/westcache/base/ExpireAfterWrite.java
+++ src/main/java/com/github/bingoohuang/westcache/base/ExpireAfterWrite.java
@@ -6,7 +6,6 @@ import java.lang.annotation.*;
  * Annotation to tag a method which returns a duration.
  *
  * @author bingoohuang [bingoohuang@gmail.com] Created on 2017/1/19.
- * @see ExpireAfterWritable
  */
 @Documented
 @Target(ElementType.METHOD)
