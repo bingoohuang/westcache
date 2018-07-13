0f44b3715e5e941a567aeb32611128702ac6c7bf
bingoo <bingoo.huang@gmail.com>
2018-07-13 10:23:57 +0800

    0.0.32-SNAPSHOT

--- src/main/java/com/github/bingoohuang/westcache/registry/RegistryTemplate.java
+++ src/main/java/com/github/bingoohuang/westcache/registry/RegistryTemplate.java
@@ -3,6 +3,7 @@ package com.github.bingoohuang.westcache.registry;
 import com.github.bingoohuang.westcache.base.WestCacheException;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * @author bingoohuang [bingoohuang@gmail.com] Created on 2016/12/24.
@@ -24,6 +25,10 @@ public class RegistryTemplate<T> {
 
     public T get(String name) {
         String key = name.isEmpty() ? "default" : name;
+        T present = registry.getIfPresent(key);
+        if (present != null) return present;
+
+        key = StringUtils.capitalize(key);
         return registry.getIfPresent(key);
     }
 }
